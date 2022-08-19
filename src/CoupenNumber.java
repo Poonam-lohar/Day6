7b6969f36e32c9d4a66fbb95e4a0974f5e0ba8cb
Your Name <you@example.com>
2022-08-19 12:50:29 +0530

    logic for Stopwatch

---
+++ src/CoupenNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CoupenNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int random = (int) (Math.random() * 10);
+            System.out.println(random);
+        }
+    }
+}
